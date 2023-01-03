99af9e8e720cfa419158785e4f108c46b6ce6b37
Alex-Stone-Github <stonealex145@gmail.com>
2023-01-02 22:06:27 -0600

    Collision With Graphics

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,49 @@
 use amath::Vec2;
 
+const WIDTH: usize = 60;
+const HEIGHT: usize = 40;
+
 fn main() {
+    // init
+    let mut render_manager = aengine2::RenderManager::new(WIDTH, HEIGHT);
+
+    // shapes
     let transform1 = Transform {
-        position: Vec2::new(0.0, 0.0),
+        position: Vec2::new(15.0, 15.0),
+        angle: std::f64::consts::FRAC_PI_2*0.0
+    };
+    let square1 = create_polygon(7, 12.0);
+    let mut transform2 = Transform {
+        position: Vec2::new(40.0, 20.0),
         angle: 0.0
     };
-    let square1 = create_polygon(4, 5.0);
-    let transform2 = Transform {
-        position: Vec2::new(5.0, 0.0),
-        angle: 0.0
-    };
-    let square2 = create_polygon(4, 1.0);
-    println!("Start");
-    println!("{:?}", apply_transform(&transform1, &square1));
-    println!("{:?}", apply_transform(&transform2, &square2));
-    println!("Done");
-    
-    println!("{}", cvpolyg_intersects_others(
-            &apply_transform(&transform1, &square1),
-            vec![&apply_transform(&transform2, &square2)]));
+    let square2 = create_polygon(4, 5.0);
+
+
+    //loop {
+        // drawing
+        render_manager.fill_background('.');
+        let new_shape1 = apply_transform(&transform1, &square1);
+        draw_shape(&mut render_manager, &new_shape1);
+        let new_shape2 = apply_transform(&transform2, &square2);
+        draw_shape(&mut render_manager, &new_shape2);
+        render_manager.present();
+        // info
+        println!("{}", cvpolyg_intersects_others(&new_shape1, vec![&new_shape2]));
+        transform2.position.x-=1.0;
+        let ten_millis = std::time::Duration::from_millis(10);
+        std::thread::sleep(ten_millis);
+    //}
+}
+fn draw_shape(rm: &mut aengine2::RenderManager, shape: &ConvexPolygon) {
+    for i in 1..shape.len() {
+        let vertex = shape[i];
+        let pvertex = shape[i-1];
+        rm.draw_line(pvertex.x as usize, pvertex.y as usize, 
+                                 vertex.x as usize, vertex.y as usize, '#');
+    }
+    rm.draw_line(shape[shape.len()-1].x as usize, shape[shape.len()-1].y as usize, 
+                             shape[0].x as usize, shape[0].y as usize, '#');
 }
 
 pub fn create_polygon(side_count: usize, side_length: f64) -> ConvexPolygon{
@@ -89,7 +114,6 @@ pub fn cvpolyg_intersects_others(primary: &ConvexPolygon, others: Vec<&ConvexPol
             let other_max = other.iter().map(|vertex| Vec2::dot(*vertex, axis)).reduce(f64::max).unwrap();
             // check for overlap - if no overlap - then intersecting = false
             if primary_max < other_min || other_max < primary_min { // if no overlap
-                println!("not intersecting");
                 intersecting = false;
             }
         }
